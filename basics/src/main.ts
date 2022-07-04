3d4372040221e1237bb9adb265e9964acb6950e9
albert18 <erertech@gmail.com>
2022-07-04 12:38:16 -0400

    update

--- basics/src/main.ts
+++ basics/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module'
 platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.error(err))
 
 
-
+/////////////////////////////////////// -- Old CODE
 // import { enableProdMode } from '@angular/core';
 // import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
