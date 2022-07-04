d57d2ebc5bdc5948d85b9301660fefc4414e683e
albert18 <erertech@gmail.com>
2022-07-04 13:04:10 -0400

    update

--- basics/src/app/app.module.ts
+++ basics/src/app/app.module.ts
@@ -1,4 +1,16 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ]
+})
+
+export class AppModule {
+
+}
 
 
 
