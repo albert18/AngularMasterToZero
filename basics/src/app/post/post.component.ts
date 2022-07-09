096612d2e240768f908fa2c0ff3b56131ed53e59
albert18 <erertech@gmail.com>
2022-07-09 13:37:47 -0400

    update

--- basics/src/app/post/post.component.ts
+++ basics/src/app/post/post.component.ts
@@ -6,8 +6,8 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent {
-  @Input() postImageUrl = ''
-  // @Input('img') postImageUrl = ''
+  // @Input() postImageUrl = ''
+  @Input('img') postImageUrl = ''
 
 
 
